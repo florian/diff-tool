998ea6e6c8efd6332d59efb12521522e5e6170e8
Florian Hartmann <jscoderr@gmail.com>
2020-12-28 12:14:14 +0100

    Implement diffing iteratively

--- diff.py
+++ diff.py
@@ -26,60 +26,69 @@ def red(content):
 def green(content):
     return _color(content, TERM_CODE_GREEN)
 
-class Differ:
-    def __init__(self, text1, text2):
-        self._text1 = text1
-        self._text2 = text2
-        self._lcs = self._compute_longest_common_subsequence()
+def _compute_longest_common_subsequence(text1, text2):
+    n = len(text1)
+    m = len(text2)
 
-    def _compute_longest_common_subsequence(self):
-        n = len(self._text1)
-        m = len(self._text2)
+    lcs = [[None for _ in range(m + 1)]
+                 for _ in range(n + 1)]
 
-        lcs = [[None for _ in range(m + 1)]
-                     for _ in range(n + 1)]
+    for i in range(0, n + 1):
+        for j in range(0, m + 1):
+            if i == 0 or j == 0:
+                lcs[i][j] = 0
+            elif text1[i - 1] == text2[j - 1]:
+                lcs[i][j] = 1 + lcs[i - 1][j - 1]
+            else:
+                lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
 
-        for i in range(0, n + 1):
-            for j in range(0, m + 1):
-                if i == 0 or j == 0:
-                    lcs[i][j] = 0
-                elif self._text1[i - 1] == self._text2[j - 1]:
-                    lcs[i][j] = 1 + lcs[i - 1][j - 1]
-                else:
-                    lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
+    return lcs
 
-        return lcs
+def diff(text1, text2):
+    lcs = _compute_longest_common_subsequence(text1, text2)
 
-    def diff(self, show_remove, show_add):
-        results = []
-        self._recursive_diff(results, show_remove, show_add, len(self._text1), len(self._text2))
+    i = len(text1)
+    j = len(text2)
 
-        for element in results:
-            if isinstance(element, Addition):
-                print(green(f"+  {element.content}"))
-            if isinstance(element, Removal):
-                print(red(f"-  {element.content}"))
-            if isinstance(element, Unchanged):
-                print("  ", element.content)
+    results = []
 
-    def _recursive_diff(self, results, show_remove, show_add, i, j):
-        if i == j == 0:
-            return
-        elif i == 0:
-            self._recursive_diff(results, show_remove, show_add, i, j - 1)
-            results.append(Addition(self._text2[j - 1]))
+    while i != 0 and j != 0:
+        # If we reached the end of text1 (i == 0) or text2 (j == 0), then we
+        # just need to print the remaining additions and removals.
+        if i == 0:
+            results.append(Addition(text2[j - 1]))
+            j -= 1
         elif j == 0:
-            self._recursive_diff(results, show_remove, show_add, i - 1, j)
-            results.append(Removal(self._text1[i - 1]))
-        elif self._text1[i - 1] == self._text2[j - 1]:
-            self._recursive_diff(results, show_remove, show_add, i - 1, j - 1)
-            results.append(Unchanged(self._text1[i - 1]))
-        elif self._lcs[i - 1][j] <= self._lcs[i][j - 1]:
-            self._recursive_diff(results, show_remove, show_add, i, j - 1)
-            results.append(Addition(self._text2[j - 1]))
+            results.append(Removal(text1[i - 1]))
+            i -= 1
+        # Otherwise there's still parts of text1 and text2 left. If the
+        # currently considered part is equal, then we found an unchanged part,
+        # which belongs to the longest common subsequence.
+        elif text1[i - 1] == text2[j - 1]:
+            results.append(Unchanged(text1[i - 1]))
+            i -= 1
+            j -= 1
+        # In any other case, we go in the direction of the longest common
+        # subsequence.
+        elif lcs[i - 1][j] <= lcs[i][j - 1]:
+            results.append(Addition(text2[j - 1]))
+            j -= 1
         else:
-            self._recursive_diff(results, show_remove, show_add, i - 1, j)
-            results.append(Removal(self._text1[i - 1]))
+            results.append(Removal(text1[i - 1]))
+            i -= 1
+
+    return reversed(results)
+
+
+
+def visualize(results, show_remove, show_add):
+    for element in results:
+        if isinstance(element, Addition):
+            print(green(f"+  {element.content}"))
+        if isinstance(element, Removal):
+            print(red(f"-  {element.content}"))
+        if isinstance(element, Unchanged):
+            print("  ", element.content)
 
 
 def _read_lines_from_file(path: str) -> List[str]:
@@ -90,8 +99,8 @@ def main():
     lines1 = _read_lines_from_file(sys.argv[1])
     lines2 = _read_lines_from_file(sys.argv[2])
 
-    differ = Differ(lines1, lines2)
-    differ.diff(show_remove=True, show_add=True)
+    diffing_result = diff(lines1, lines2)
+    visualize(diffing_result, show_remove=True, show_add=True)
 
 if __name__ == '__main__':
     main()
